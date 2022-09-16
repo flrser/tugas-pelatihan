4a5cbdc9c55c361d565a8daa12767c87a0da5dd6
flrser <113252009+flrser@users.noreply.github.com>
2022-09-16 19:54:54 +0700

    Add files via upload

---
+++ florisedelweis.py
@@ -0,0 +1,47 @@
+celcius = 50
+F = 9/5*celcius+32
+print("hasil celcius ke fahrenheit=", F, "F")
+
+celcius = 40
+R = 4/5*celcius
+print("hasil celcius ke reamur=", R, "R")
+
+celcius = 75
+K = celcius+273
+print("hasil celcius ke kelvin=", K, "K")
+
+fahrenheit = 86
+C = 5/9*fahrenheit-32
+print("hasil farenhit ke celcius=", 5/9*54, "C")
+
+fahrenheit = 122
+R = 4/9*fahrenheit-32
+print("hasil farenhit ke reamur=", 4/9*180, "R")
+
+fahrenheit = 122
+K = 5/9*fahrenheit-32+273
+print("hasil farenhit ke kelvin=", 5/9*90+273, "K")
+
+reamur = 36
+C = 5/4*reamur
+print("hasil reamur ke celcius=", C, "C")
+
+reamur = 36
+F = 9/4*reamur+32
+print("hasil reamur ke fahrenheit=", F, "F")
+
+reamur = 36
+K = 5/4*reamur+273
+print("hasil reamur ke kelvin=", K, "K")
+
+kelvin = 333
+C = kelvin-273
+print("hasil kelvin ke celcius=", C, "C")
+
+kelvin = 20
+F = 9/5*60+32
+print("hasil kelvin ke fahrenheit=", F, "F")
+
+kelvin = 333
+R = 4/5*60
+print("hasil kelvin ke reamur=", R, "R")
